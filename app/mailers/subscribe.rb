47ac8a203e78e67f8722c8b0ed1e785586c7adec
Nazariy Papizh <nazariy.papizh@gmail.com>
2016-04-29 18:13:37 +0300

    t

--- app/mailers/subscribe.rb
+++ app/mailers/subscribe.rb
@@ -2,12 +2,12 @@ class Subscribe < ApplicationMailer
 
   def become_a_dealer(data)
     @data = data
-    mail( :layout => false, :subject => "#{@data[:name]}, забажав стати диллером ...", :to => 'nazariy.papizh@gmail.com')
+    mail( :layout => false, :subject => "#{@data[:name]}, забажав стати диллером ...", :to => 'starbud@ukr.net')
   end
 
   def contact_us(data)
     @data = data
-    mail( :layout => false, :subject => "#{@data[:name]}, написав нам ...", :to => 'nazariy.papizh@gmail.com')
+    mail( :layout => false, :subject => "#{@data[:name]}, написав нам ...", :to => 'starbud@ukr.net')
   end
 
 end
