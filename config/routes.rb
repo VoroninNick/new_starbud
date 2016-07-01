d3897e92f74179c41e7ae67693a7c92372a0525b
Nazariy <nazariy.papizh@gmail.com>
2016-07-01 16:42:41 +0300

    added success respond

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post 'become-a-dealer' => 'main#become_a_dealer'
   post 'contact-us' => 'main#contact_us'
 
-  post '*args' => 'application#listener_1c'
+  match '*args' => 'application#listener_1c', via: [:post, :get, :put]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
